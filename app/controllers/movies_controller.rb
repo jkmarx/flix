72280bd65544c32c70f81b697043f1b95a87533a
jkmarx <jennyrutledge@gmail.com>
2023-12-24 18:50:40 -0500

    Chapter 17 Add seeds and more fields to movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,7 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :released_on, :rating, :total_gross)
+    params.require(:movie).permit(:title, :description, :released_on, :rating, :total_gross, :director, :duration, :image_file_name)
   end
 
 end
