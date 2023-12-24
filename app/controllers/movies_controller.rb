70fb73e8a4317abe32107e17df6711ff012b2405
jkmarx <jennyrutledge@gmail.com>
2023-12-24 15:38:50 -0500

    Chapter 14 Creating records and refactoring to use partials.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,20 +3,33 @@ class MoviesController < ApplicationController
     @movies =  Movie.all
   end
 
-  def show
+  def edit
     @movie = Movie.find(params[:id])
   end
 
-  def edit
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
     @movie = Movie.find(params[:id])
   end
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :description, :released_on, :rating, :total_gross)
-
-    @event.update(movie_params)
+    @movie.update(movie_params)
     redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :released_on, :rating, :total_gross)
   end
 
 end
