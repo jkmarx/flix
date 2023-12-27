5b39f3e8e05377b94df800086a79bf83fa712c9b
jkmarx <jennyrutledge@gmail.com>
2023-12-27 09:08:30 -0500

    Chapter 33 Add admin to users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   private
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
   def current_user
     @current_user || User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
   end
 
   def require_signin
@@ -11,11 +24,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_user?(user)
-    current_user == user
-  end
-
   helper_method :current_user?
-
   helper_method :current_user
+  helper_method :current_user_admin?
 end
